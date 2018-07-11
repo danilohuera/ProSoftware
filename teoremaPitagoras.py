255aa2f72ac2c40999af97920d878b6fa27803d5
danilohuera <40905229+danilohuera@users.noreply.github.com>
2018-07-11 00:52:59 -0500

    Add files via upload

---
+++ teoremaPitagoras.py
@@ -0,0 +1,9 @@
+
+print (" Teorema de Pitagoras")
+print (" Al ingresar los valores de los catetos")
+cata= float(input(" Ingrese cateto a: "))
+catb= float(input(" Ingrese cateto b: "))
+hipc= (cata**2 + catb**2)**0.5
+print(" Se aplica la formula de teorema de Pitagoras para \n obtener la Hipotenusa c")
+print ( "el valor de la hipotenusa c es:",hipc)         
+
